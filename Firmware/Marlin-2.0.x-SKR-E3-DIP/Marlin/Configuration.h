75ca8d4c22bb69ec49e253b1e0235bc85540f5e7
Wouter Snels <ofloo@h4ck.io>
2019-08-11 17:22:18 +0200

    Fixed under extrusion Changed default voltage to 24v because ender3 pro runs on 24v

--- Firmware/Marlin-2.0.x-SKR-E3-DIP/Marlin/Configuration.h
+++ Firmware/Marlin-2.0.x-SKR-E3-DIP/Marlin/Configuration.h
@@ -720,7 +720,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 104 }
 
 /**
  * Default Max Feed Rate (mm/s)
