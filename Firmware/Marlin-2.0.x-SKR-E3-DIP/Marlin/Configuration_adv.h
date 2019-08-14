8060dbd599a42a2e6002e811886d769eb14a90c3
Wouter Snels <ofloo@h4ck.io>
2019-08-14 20:10:46 +0200

    Got new fans for extruder 12v/24v and they make a nice hole in the water at 90 so why make them noisy and run them at 255

--- Firmware/Marlin-2.0.x-SKR-E3-DIP/Marlin/Configuration_adv.h
+++ Firmware/Marlin-2.0.x-SKR-E3-DIP/Marlin/Configuration_adv.h
@@ -301,8 +301,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-//#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MIN_PWM 0
+#define FAN_MAX_PWM 90
 
 /**
  * FAST PWM FAN Settings
